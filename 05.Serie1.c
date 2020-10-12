2bc1374881a698e37033f69aac0192da7d3f53c2
ErnestoMondragon <72045701+ErnestoMondragon@users.noreply.github.com>
2020-10-12 17:05:44 -0500

    Add files via upload

---
+++ 05.Serie1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main(void)
+{
+int resultado=0;
+int a;
+int resul;
+int resul1;
+{
+printf("Escribe un número:\n");
+  scanf("%d", &a);
+resul=a*3;
+resul1=a*7+2;
+}
+
+{
+for(resultado=resul; resultado<=resul1; resultado=resultado+2)
+printf("%i,",resultado);
+
+}
+
+}
---
+++ 06.Serie2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main(void)
+{
+int resultado=0;
+int a;
+int resul;
+int resul1;
+{
+printf("Escribe un número:\n");
+  scanf("%d", &a);
+resul=a*a;
+resul1=-1;
+}
+
+{
+for(resultado=resul; resultado>=resul1; resultado=resultado-2)
+printf("%i,",resultado);
+
+}
+
+}
